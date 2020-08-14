7701977bd26afa594f11cc243badeeaff5ca1a6b
Edinson Santa Cruz <edinsonlen@hotmail.com>
2020-08-14 13:52:20 -0500

    first up

---
+++ store/reducers/product.js
@@ -0,0 +1,10 @@
+import PRODUCT from '../../models/product';
+
+const initialState = {
+    availableProducts: [],
+    userProducts: []
+};
+
+export default (state=initialState, action) => {
+    return state;
+}; 
