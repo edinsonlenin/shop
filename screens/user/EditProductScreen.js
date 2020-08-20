e4ab0e59e64038fb579acdb417ecc260b673fc69
edinsonlenin <edinsonlen@hotmail.com>
2020-08-20 16:56:31 -0500

    edit product screen

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,59 +1,67 @@
-import React, { route } from "react";
+import React, { route, useState } from "react";
 import { View, StyleSheet, Text, ScrollView, TextInput } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "../../components/UI/HeaderButton";
 
 const EditProductScreen = ({ route, navigation }) => {
+  const { productId } = route.params ?? {};
+  const [title, setTitle] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      title: route.params ? 'Edit Product' : 'Add Product',
-    //   headerRight: () => (
-    //     <HeaderButtons HeaderButtonComponent={HeaderButton}>
-    //       <Item
-    //         title="Cart"
-    //         iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
-    //         onPress={() => {
-    //           navigation.navigate("Cart");
-    //         }}
-    //       />
-    //     </HeaderButtons>
-    //   ),
-    //   headerLeft: () => (
-    //     <HeaderButtons HeaderButtonComponent={HeaderButton}>
-    //       <Item
-    //         title="Menu"
-    //         iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
-    //         onPress={() => {
-    //           navigation.toggleDrawer();
-    //         }}
-    //       />
-    //     </HeaderButtons>
-    //   ),
+      title: productId ? "Edit Product" : "Add Product",
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Save"
+            iconName={
+              Platform.OS === "android" ? "md-checkmark" : "ios-checkmark"
+            }
+            onPress={() => {}}
+          />
+        </HeaderButtons>
+      ),
+      //   headerLeft: () => (
+      //     <HeaderButtons HeaderButtonComponent={HeaderButton}>
+      //       <Item
+      //         title="Menu"
+      //         iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+      //         onPress={() => {
+      //           navigation.toggleDrawer();
+      //         }}
+      //       />
+      //     </HeaderButtons>
+      //   ),
     });
   }, [navigation]);
-
-  let productId;
-  console.log(route.params);
-  if (route.params) {
-   let { productId } = route.params;
-  }
- console.log(productId, "productId");
   return (
     <ScrollView>
       <View style={styles.form}>
         <View style={styles.formControl}>
           <Text style={styles.label}>Title</Text>
-          <TextInput style={styles.input} />
+          <TextInput
+            style={styles.input}
+            value={title}
+            onChangeText={(text) => setTitle(text)}
+          />
         </View>
         <View style={styles.formControl}>
           <Text style={styles.label}>Image Url</Text>
-          <TextInput style={styles.input} />
+          <TextInput style={styles.input} value={imageUrl}
+            onChangeText={(text) => setImageUrl(text)}/>
         </View>
         <View style={styles.formControl}>
           <Text style={styles.label}>Price</Text>
-          <TextInput style={styles.input} />
+          <TextInput style={styles.input} value={price}
+            onChangeText={(text) => setPrice(text)}/>
         </View>
         <View style={styles.formControl}>
           <Text style={styles.label}>Description</Text>
-          <TextInput style={styles.input} />
+          <TextInput style={styles.input} value={description}
+            onChangeText={(text) => setDescription(text)}/>
         </View>
       </View>
     </ScrollView>
@@ -62,20 +70,20 @@ const EditProductScreen = ({ route, navigation }) => {
 
 const styles = StyleSheet.create({
   form: {
-    margin: 20
+    margin: 20,
   },
   formControl: {
-    width: '100%'
+    width: "100%",
   },
   label: {
-    fontFamily: 'open-sans-bold',
-    marginVertical: 8
+    fontFamily: "open-sans-bold",
+    marginVertical: 8,
   },
   input: {
     paddingHorizontal: 2,
     paddingVertical: 5,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
   },
 });
 
